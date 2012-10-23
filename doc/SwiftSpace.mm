e493b2ea0d2a630dc42dbba98c1baa1120dca2fc
Robert Giacinto <robert.giacinto@gmail.com>
2012-10-23 16:14:11 +0200

    initial commit

---
+++ doc/SwiftSpace.mm
@@ -0,0 +1,120 @@
+<map version="1.0.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1350999076258" ID="ID_809622003" MODIFIED="1351001537829" TEXT="SwiftSpace">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ol>
+      <li>
+        Iteration:
+
+        <ol>
+          <li>
+            Simulation Singleton
+
+            <ol>
+              <li>
+                Structure Management
+              </li>
+              <li>
+                Agent Management
+              </li>
+              <li>
+                Simulation Clock
+              </li>
+            </ol>
+            <p>
+              
+            </p>
+          </li>
+          <li>
+            Agenten
+
+            <ol>
+              <li>
+                Haben Eigenschaften
+              </li>
+              <li>
+                Haben Gesundheit
+              </li>
+              <li>
+                Haben Aufgaben / AI
+              </li>
+            </ol>
+            <p>
+              
+            </p>
+          </li>
+          <li>
+            Structures
+
+            <ol>
+              <li>
+                Alles, was gebaut werden kann und aus Modulen besteht
+              </li>
+              <li>
+                Haben grundlegende Werte und Eigenschaften (Masse, Gr&#246;&#223;e etc.)
+              </li>
+            </ol>
+          </li>
+        </ol>
+      </li>
+    </ol>
+  </body>
+</html>
+</richcontent>
+<font NAME="Source Code Pro" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1351000374075" ID="ID_245719283" MODIFIED="1351000395858" POSITION="right" TEXT="Raumstationen">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="Source Code Pro" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1351000398977" ID="ID_1983120308" MODIFIED="1351000454452" TEXT="Module">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Source Code Pro" SIZE="16"/>
+<node COLOR="#990000" CREATED="1351000455982" ID="ID_886958233" MODIFIED="1351000474319" TEXT="bieten Funktionalit&#xe4;ten an, die von Agenten genutzt werden k&#xf6;nnen">
+<font NAME="Source Code Pro" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1351000477523" ID="ID_646538279" MODIFIED="1351000494456" TEXT="k&#xf6;nnen in die Raumstation eingebaut werden">
+<font NAME="Source Code Pro" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1351000513970" ID="ID_1641124339" MODIFIED="1351000525989" TEXT="k&#xf6;nnen von Agenten betreten werden">
+<font NAME="Source Code Pro" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1351000535563" ID="ID_573590915" MODIFIED="1351000543963" TEXT="haben einen Namen">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Source Code Pro" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1351000545591" ID="ID_1017419989" MODIFIED="1351000552445" TEXT="haben eine Gr&#xf6;&#xdf;e">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Source Code Pro" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1351000555545" ID="ID_1877079509" MODIFIED="1351000567976" TEXT="haben unterschiedliche Kapazit&#xe4;ten">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Source Code Pro" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1351000569968" ID="ID_1688390494" MODIFIED="1351000591424" TEXT="k&#xf6;nnen bewohnt werden (abh&#xe4;ngig von Modul)">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Source Code Pro" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1351000901394" ID="ID_1375332770" MODIFIED="1351000905506" POSITION="left" TEXT="Agenten">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="Source Code Pro" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1351000907497" ID="ID_1316052097" MODIFIED="1351000934195" TEXT="k&#xf6;nnen sich in der Welt bewegen">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Source Code Pro" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1351000934996" ID="ID_1731707530" MODIFIED="1351000946955" TEXT="existieren entweder in Raumstationen oder Raumschiffen">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Source Code Pro" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1351000947429" ID="ID_1129850463" MODIFIED="1351000952614" TEXT="haben einen Beruf, dem sie folgen">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Source Code Pro" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
