9ead140570c61acc94c61fcc9485ff25c7fb1d32
Robert Giacinto <robert.giacinto@gmail.com>
2012-10-25 12:39:50 +0200

    modules added to structure

--- swiftspace-core/src/main/scala/net/swiftspace/core/Building.scala
+++ swiftspace-core/src/main/scala/net/swiftspace/core/Building.scala
@@ -1,47 +1,57 @@
 package net.swiftspace.core
 
 import akka.actor.Actor
+import collection.mutable
+import akka.event.Logging
 
-/**
- * Every larger structure that can be inhabitable.
- */
-class Building(override val name: String, override val coordinate: Coordinate) extends Structure(name, coordinate) {
-  def receive = {
-    case Tick() =>
-    // Update modules
-  }
-}
 
 /**
  * Is part of a structure and can fulfil a service or function.
  */
-class StructureModule extends Actor {
-  def receive = {
-    case Tick =>
-      // Update module
+class StructureModule(val requirements: ModuleRequirements) {
+  def update() = {
+
   }
 }
 
-abstract class Structure(val name: String, val coordinate: Coordinate) extends Actor{
+case class ModuleRequirements(energy: Double)
+
+case class RegisterModule(module: StructureModule)
+
+/**
+ * A structure is something that can be build, used or inhabited by the persons inside the simulation.
+ * @param name the name of the structure
+ * @param coordinate the coordinate in space
+ */
+class Structure(val name: String, val coordinate: Coordinate) extends Actor {
+  val log = Logging(context.system, this)
   var energyAvailable = 0.0
   var energyUsed = 0.0
-  var energyNeeded = 0.0
+
+  var modules = mutable.Buffer[StructureModule]()
+
+  def receive = {
+    case RegisterModule(module) =>
+      if (energyAvailable >= module.requirements.energy) {
+        modules += module
+        energyAvailable -= module.requirements.energy
+        energyUsed += module.requirements.energy
+      } else {
+        log.error("Unsufficient energy suplly to install new module")
+      }
+    case Tick() =>
+      modules.foreach(m => m.update())
+  }
 }
 
-import StructureType._
-
-case class GetNearestStructure(structureType: StructureType)
+case class CreateStructure()
 
 class StructureManager extends Actor {
   def receive = {
     case Tick() =>
     // Update Structures
+    case CreateStructure() =>
+
 
   }
-}
-
-
-object StructureType extends Enumeration {
-  type StructureType = Value
-  val STATION = Value
 }
