19363356ef593411ec90b2e47d6d4b29cdd6020c
Robert Giacinto <robert.giacinto@gmail.com>
2012-10-25 12:54:33 +0200

    modules need resources

--- swiftspace-core/src/main/scala/net/swiftspace/core/Building.scala
+++ swiftspace-core/src/main/scala/net/swiftspace/core/Building.scala
@@ -6,17 +6,41 @@ import akka.event.Logging
 
 
 /**
- * Is part of a structure and can fulfil a service or function.
+ * A module is part of a structure in space. It can be installed and fulfill any kind of service.
+ *
+ * @param requirements the requirements of this module
+ * @param input the resource that is being processed by this module unit
+ * @param output the product that is being produced by this module unit
  */
-class StructureModule(val requirements: ModuleRequirements) {
+class StructureModule(val requirements: ModuleRequirements, val input: ModuleInput, val output: ModuleOutput) {
   def update() = {
 
   }
 }
 
+/**
+ * The requirements a Structure has to meet to be able to install the module
+ * @param energy
+ */
 case class ModuleRequirements(energy: Double)
 
-case class RegisterModule(module: StructureModule)
+/**
+ * What is processed by the module?
+ * @param resource the name of the resource
+ */
+case class ModuleInput(resource: String)
+
+/**
+ * What is produced by the module?
+ * @param resource the name of the resource
+ */
+case class ModuleOutput(resource: String)
+
+/**
+ * Installs a new module to the structure.
+ * @param module the module being installed
+ */
+case class InstallModule(module: StructureModule)
 
 /**
  * A structure is something that can be build, used or inhabited by the persons inside the simulation.
@@ -31,7 +55,7 @@ class Structure(val name: String, val coordinate: Coordinate) extends Actor {
   var modules = mutable.Buffer[StructureModule]()
 
   def receive = {
-    case RegisterModule(module) =>
+    case InstallModule(module) =>
       if (energyAvailable >= module.requirements.energy) {
         modules += module
         energyAvailable -= module.requirements.energy
